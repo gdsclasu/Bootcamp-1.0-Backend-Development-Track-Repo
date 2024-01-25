169e2a8f5e58010b4100766844f5b935e331fe42
Tijani Ahmad <omegabliss23@gmail.com>
2024-01-25 20:36:52 +0100

    Code: added last code change

--- Introduction_to_javascript/objects.js
+++ Introduction_to_javascript/objects.js
@@ -1,5 +1,5 @@
-// Syntax: const object = {key: value} - key/value pairs
-// keys, values, entries
+// // Syntax: const object = {key: value} - key/value pairs
+// // keys, values, entries
 
 const person = {
     firstName: "John",
@@ -15,36 +15,55 @@ const person = {
     }
 }
 
-// accessing items in an object
-// object.key
+// // accessing items in an object
+// // object.key
 
-console.log(person.firstName)
-console.log(person.lastName)
-console.log(person.age)
-console.log(person.address)
-console.log(person.getFullName())
+// console.log(person.firstName)
+// console.log(person.lastName)
+// console.log(person.age)
+// console.log(person.address)
+// console.log(person.getFullName())
 
-// Get keys in objects
-const keys = Object.keys(person)
-console.log(keys)
+// // Get keys in objects
+// const keys = Object.keys(person)
+// console.log(keys)
 
-// // Get values in objects
-const values = Object.values(person)
-console.log(values)
+// // // Get values in objects
+// const values = Object.values(person)
+// console.log(values)
 
-// // Get all entries in objects - person or Object.entries(person)
-const entries = Object.entries(person)
-console.log(entries)
+// // // Get all entries in objects - person or Object.entries(person)
+// const entries = Object.entries(person)
+// console.log(entries)
 
-// Destructuring objects
-const { firstName, lastName, address } = person
+// // Destructuring objects
+// const { firstName, lastName, address } = person
 
-console.log(firstName)
-console.log(lastName)
-console.log(address)
+// console.log(firstName)
+// console.log(lastName)
+// console.log(address)
 
-// spread operator in objects
-const { firstname, ...otherInfo } = person
+// // spread operator in objects
+// const { firstname, ...otherInfo } = person
 
-console.log(firstname)
-console.log(otherInfo)
+// console.log(firstname)
+// console.log(otherInfo)
+
+// interation of objects
+// for (items in person) {
+//     console.log(`The items are ${person[items]}`)
+// }
+
+// Serialization of objects
+console.log(person)
+
+console.log("========================")
+
+let personJSON = JSON.stringify(person)
+
+console.log(personJSON)
+
+console.log("========================")
+// Deserialization of Objects
+let normalObject = JSON.parse(personJSON)
+console.log(normalObject)
